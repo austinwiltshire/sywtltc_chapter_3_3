73e73548c0fd3976db9fb9cc9703929464aa794f
emdavis7 <ericd@simpli.fi>
2016-12-22 11:10:46 -0600

    Fail median test

--- stats_test.py
+++ stats_test.py
@@ -5,6 +5,8 @@ import stats
 A = [2, 4, 6]
 B = [10, 20]
 C = [50, 100, 180, 70]
+D = [1, 2, 3, 4, 5]
+E = [10, 20, 50, 80, 100]
 
 
 def test_mean():
@@ -12,3 +14,9 @@ def test_mean():
     assert stats.mean(A) == 4.0
     assert stats.mean(B) == 15.0
     assert stats.mean(C) == 100.0
+
+def test_median():
+    """Tests the median function"""
+    assert stats.median(A) == 4.0
+    assert stats.median(D) == 3.0
+    assert stats.median(E) == 50
