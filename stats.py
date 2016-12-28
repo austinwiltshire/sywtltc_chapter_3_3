0f122ec9ef16019b7965e21c68f30cf957ca28b6
emdavis7 <ericd@simpli.fi>
2016-12-28 11:12:20 -0600

    Change variable name, add more postconditions

--- stats.py
+++ stats.py
@@ -4,61 +4,67 @@ import numbers
 import collections
 import statistics
 
-def mean(data):
+def mean(number_list):
     """Takes the mean/average of a list of numbers, a simple measure of 'middle'.
     See: https://en.wikipedia.org/wiki/Arithmetic_mean"""
     #preconditions
-    assert isinstance(data, collections.Iterable), "Must be iterable."
-    for num in data:
+    assert isinstance(number_list, collections.Iterable), "Must be iterable."
+    for num in number_list:
         assert isinstance(num, numbers.Number), "We need numbers yo."
-    assert len(data) > 1, "Please enter at least 2 numbers to be averaged."
+    assert len(number_list) > 1, "Please enter at least 2 numbers to be averaged."
     #function
-    answer = (sum(data) / len(data))
+    answer = (sum(number_list) / len(number_list))
     #postconditions
     assert isinstance(answer, numbers.Number), "Mean will return a number."
+    assert answer > min(number_list), "Mean should be higher than lowest number in list."
+    assert answer < max(number_list), "Mean should be lower than highest number in list."
     return answer
 
-def median(data):
+def median(number_list):
     """Finds the median or middle of a list of sorted numbers, a simple measure of 'middle'.
     See: https://en.wikipedia.org/wiki/Median"""
     #preconditions
-    assert isinstance(data, collections.Iterable), "Must be iterable."
-    for num in data:
+    assert isinstance(number_list, collections.Iterable), "Must be iterable."
+    for num in number_list:
         assert isinstance(num, numbers.Number), "We need numbers yo."
-    assert len(data) > 2, "Please enter at least 3 numbers to calculate median."
-    assert sorted(data) == data, "Numbers must be in order smallest to largest."
+    assert len(number_list) > 2, "Please enter at least 3 numbers to calculate median."
+    assert sorted(number_list) == number_list, "Numbers must be in order smallest to largest."
     #function
-    answer = statistics.median(data)
+    answer = statistics.median(number_list)
     #postconditions
     assert isinstance(answer, numbers.Number), "Median will return a number."
+    assert answer > min(number_list), "Median should be higher than lowest number in list."
+    assert answer < max(number_list), "Median should be lower than highest number in list."
     return answer
 
-def range_(data):
+def range_(number_list):
     """Finds ths range of a list of numbers, a simple measure of variance.
     See: https://www.mathsisfun.com/definitions/range-statistics-.html"""
     #preconditions
-    assert isinstance(data, collections.Iterable), "Must be iterable."
-    for num in data:
+    assert isinstance(number_list, collections.Iterable), "Must be iterable."
+    for num in number_list:
         assert isinstance(num, numbers.Number), "We need numbers yo."
-    assert len(data) > 1, "Please enter at least 2 numbers to calculate range."
+    assert len(number_list) > 1, "Please enter at least 2 numbers to calculate range."
     #function
-    answer = max(data) - min(data)
+    answer = max(number_list) - min(number_list)
     #postcondition
     assert isinstance(answer, numbers.Number), "Range will return a number."
-    assert max(data) - answer == min(data), "The max  minus the answer should = lowest number."
+    assert max(number_list) - answer == min(number_list), "The max - answer should = lowest number"
     return answer
 
-def standard_deviation(data):
+def standard_deviation(number_list):
     """Finds the standard deviation of a list of numbers, a measure of variance.
-    See: https://www.mathsisfun.com/data/standard-deviation-formulas.html"""
+    See: https://www.mathsisfun.com/number_list/standard-deviation-formulas.html"""
     #preconditions
-    assert isinstance(data, collections.Iterable), "Must be iterable."
-    for num in data:
+    assert isinstance(number_list, collections.Iterable), "Must be iterable."
+    for num in number_list:
         assert isinstance(num, numbers.Number), "We need numbers yo."
-    assert len(data) > 1, "Please enter at least 2 numbers to calculate standard deviation."
+    assert len(number_list) > 1, "Please enter at least 2 numbers to calculate standard deviation."
     #function
-    answer = statistics.stdev(data)
+    answer = statistics.stdev(number_list)
     rounded_answer = round(answer, 5)
     #postconditions
     assert isinstance(rounded_answer, numbers.Number), "Standard Deviation will return a number."
+    assert rounded_answer > min(number_list), "SD should be higher than lowest number in list."
+    assert rounded_answer < max(number_list), "SD should be lower than highest number in list."
     return rounded_answer
